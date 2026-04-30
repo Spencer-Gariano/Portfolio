5e4fbc49a406f9abc33335623c1572d8dc4fc685
Spencer Gariano <71291359+Spencer-Gariano@users.noreply.github.com>
2026-04-30 13:59:29 -0700

    Restrcutured src and added oxc
    OXC for linting and formatting, as well as added a components and application folder to sort the source code more

---
+++ oxfmt.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'oxfmt';
+
+export default defineConfig({
+  useTabs: false,
+  tabWidth: 2,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  sortTailwindcss: { type: true },
+});
