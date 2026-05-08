51556e0f194a109676cfeb5e06abf5d30cdab49c
Spencer Gariano <71291359+Spencer-Gariano@users.noreply.github.com>
2026-05-08 16:59:24 -0700

    Removed deprecated code and added https

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,14 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: 'local.spencergariano.io',
+    port: 5172,
+    https: {
+      key: '../certs/local.spencergariano.io-key.pem',
+      cert: '../certs/local.spencergariano.io.pem',
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
